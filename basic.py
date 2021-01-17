0e9ce82a56adeeda5ce12c654e9b695e04a604d9
Juan Cruz <jc23729@gmail.com>
2021-01-17 21:27:52 +0000

    update basic.py

--- basic.py
+++ basic.py
@@ -1,1 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
