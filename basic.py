f024409f99eb6dbe2f74f6eb7da5aae406d5f8cc
Juan Cruz <jc23729@gmail.com>
2021-01-17 21:58:21 +0000

    ClassInfoForm

--- basic.py
+++ basic.py
@@ -5,3 +5,10 @@ from wtforms import StringField, SubmitField
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecretkey'
+
+class InfoForm(FlaskForm):
+
+  breed = StringField("What Breed are you?")
+  submit = SubmitField('Submit')
+
+  
